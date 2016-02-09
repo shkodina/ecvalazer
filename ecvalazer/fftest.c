ae06de0c4ce3ac9a6e389ef75e821ace2f0b8d4c
shkodina <blobby@yandex.ru>
2016-02-09 22:54:43 +0300

    готовая прошивка

--- ecvalazer/fftest.c
+++ ecvalazer/fftest.c
@@ -7,13 +7,15 @@
 #include "ffft.h"		/* Defs for using Fixed-point FFT module */
 #include <avr/delay.h>
 #include <stdio.h>
+#include "common_makros.h"
 
 #define	SYSCLK		8000000
 
 
-
-/*------------------------------------------------*/
-/* Global variables                               */
+#include <avr/interrupt.h>   
+//======================================================================================
+//        Global variables                               */
+//======================================================================================
 
 char pool[16];	/* Console input buffer */
 
@@ -23,9 +25,9 @@ uint16_t spektrum[FFT_N/2];		/* Spectrum output buffer */
 
 
 
-/*------------------------------------------------*/
-/* Capture waveform                               */
-
+//======================================================================================
+//         Capture waveform                               */
+//======================================================================================
 void capture_wave (int16_t *buffer, uint16_t count)
 {
 	ADMUX = _BV(REFS0)|_BV(ADLAR)|_BV(MUX2)|_BV(MUX1)|_BV(MUX0);	// channel
@@ -44,104 +46,188 @@ void capture_wave (int16_t *buffer, uint16_t count)
 	ADCSRA = 0;
 }
 
+//=====================================================================================
 
-/* This is an alternative function of capture_wave() and can omit captureing buffer.
-
-void capture_wave_inplace (complex_t *buffer, uint16_t count)
+void ledOff()
 {
-	const prog_int16_t *window = tbl_window;
-	int16_t v;
+	PORTC = 0;
+	PORTD &= ~(0b11111100);
 
-	ADMUX = _BV(REFS0)|_BV(ADLAR)|_BV(MUX2)|_BV(MUX1)|_BV(MUX0);	// channel
-
-	do {
-		ADCSRA = _BV(ADEN)|_BV(ADSC)|_BV(ADFR)|_BV(ADIF)|_BV(ADPS2)|_BV(ADPS1);
-		while(bit_is_clear(ADCSRA, ADIF));
-		v = fmuls_f(ADC - 32768, pgm_read_word_near(window));
-		buffer->r = v;
-		buffer->i = v;
-		buffer++; window++;
-	} while(--count);
-
-	ADCSRA = 0;
 }
-*/
 
-/*------------------------------------------------*/
-/* Online Monitor via an ISP cable                */
+//=====================================================================================
+
+void setChannel(char n)
+{
+	//PORTB &= 0b11100000;
+	//PORTA &= 0b11100000;
+	PORTB = 0;
+	PORTA = 0;
+
+
+	switch (n){
+		case 0:  UPBIT(PORTB, 4); break;
+		case 1:  UPBIT(PORTB, 3); break;
+		case 2:  UPBIT(PORTB, 2); break;
+		case 3:  UPBIT(PORTB, 1); break;
+		case 4:  UPBIT(PORTB, 0); break;
+		case 5:  UPBIT(PORTA, 0); break;
+		case 6:  UPBIT(PORTA, 1); break;
+		case 7:  UPBIT(PORTA, 2); break;
+		case 8:  UPBIT(PORTA, 3); break;
+		case 9:  UPBIT(PORTA, 4); break;
+		default: break;
+
+	}
+
+}
+
+//=====================================================================================
+
+void ledOn(char s)
+{
+
+	//PORTC = 0b10000000;
+	//return;
+	
+
+	if (s == 0) return;
+
+	if (s <= 8){
+		PORTC = (0xff << (8 - s));
+		return;
+	} 
+
+
+	PORTC = 0xff;
+	PORTD |= (0b11111100 << (6 - (s - 8)));
+
+	return;
+
+}
+
+//======================================================================================
+
+void inIRQTimer(){
+	#define CHK 10
+	char channels[CHK] = {1,2,3,5,7,10,16,29,57,62};
+
+	uint16_t s;
+	static uint16_t n = CHK;
+
+	if (n++ >= CHK)
+		n = 0;
+
+
+	//loggerWrite("\n\r",2);
+	//loggerWrite("\n\r",2);
+
+	s = spektrum[channels[n]] ;
+	//loggerWrite("\n\r",2);
+	//s /= FFT_N;
+		
+	if(s >= 1){
+		if (s > 2){
+			if (s > 4){
+				if (s > 7){
+					if (s > 10){
+						if (s > 14){
+							if ( s > 20){
+								if (s > 30){
+									if (s > 55){
+										if ( s > 70){
+											if (s > 100){
+												if (s > 135){
+													if (s > 155){
+														if ( s > 180){
+															s = 14;
+														} else s = 13;
+													} else s = 12;
+												} else s = 11;
+											} else s = 10;
+										} else s = 9;
+									} else s = 8;
+								} else s = 7;
+							} else s = 6;
+						} else s = 5;
+					} else s = 4;
+				} else s = 3;
+			} else s = 2;
+		} else s = 1;
+	} else s = 0;
+
+					
+	//for (m = 0; m < s; m++) loggerWrite("**",1);
+
+	ledOff();
+	setChannel(n);
+	ledOn(s);
+
+
+}
+
+//======================================================================================
+
+ISR (TIMER1_OVF_vect)
+{
+    inIRQTimer();
+    // run timer
+    TCNT1 = 65536 - 1; //  31220;
+    TCCR1B = (1<<CS12 | 1<<CS10);
+    TIMSK |= (1<<TOIE1);
+
+
+}
+
+//=====================================================
+
+void initTimer()
+{
+	TCNT1 = 65536-1;
+    TCCR1B = (1<<CS12 | 1<<CS10);
+    TIMSK |= (1<<TOIE1);
+    sei();
+
+
+}
+
+//=====================================================
+
+void initLeds()
+{
+	PORTC = 0;
+	DDRC = 0xff;
+
+	PORTD &= ~(0b11111100);
+	DDRD |= 0b11111100;
+
+	PORTB &= ~(0b00011111);
+	DDRB |= 0b00011111;
+
+	PORTA &= ~(0b00011111);
+	DDRA |= 0b00011111;
+
+}
+
+//=====================================================
 
 int main (void)
 {
-	char cp;
-	uint16_t m, n, s;
-	uint16_t t1,t2,t3;
+	initTimer();
+	initLeds();
 
-
-	//DDRE = 0b00000010;	/* PE1:<conout>, PE0:<conin> in N81 38.4kbps */
-	//TCCR1B = 3;	/* clk/64 */
 
 	loggerInit();
 	loggerWriteToMarker((LogMesT)"\r\nFFT sample program\r\n*", '*');
 	loggerWriteToMarker((LogMesT)"\r\n>*", '*');			/* Prompt */
 	for(;;) {
 		
-//		rcvrstr(pool, sizeof(pool));	/* Console input */
-//		cp = pool;
-		cp = 's';
-
-		switch (cp) {	/* Pick a header char (command) */
-			case '\0' :		/* Blank line */
-				break;
-
-			case 'w' :		/* w: show waveform */
-				capture_wave(capture, FFT_N);
-				for (n = 0; n < FFT_N; n++) {
-					s = capture[n];
-					//xmitf(PSTR("\r\n%4u:%6d "), n, s);
-					s = (s + 32768) / 1024;
-					//for (m = 0; m < s; m++) xmit(' ');
-					//xmit('*');
-				}
-				break;
-
-			case 's' :		/* s: show spectrum */
-				loggerWriteToMarker((LogMesT)"show spectrum\r\n>*", '*');
-				capture_wave(capture, FFT_N);
-				/*
-				for(char i = 0; i < FFT_N; i++){
-					char str[20];
-					sprintf(str, "%d\n\r*",capture[i]);
-						loggerWriteToMarker((LogMesT)str, '*');
-						
-
-				}
-				*/
+		capture_wave(capture, FFT_N);
 				
-				TCNT1 = 0;	/* performance counter */
-				fft_input(capture, bfly_buff);
-				//t1 = TCNT1; TCNT1 = 0;
-				fft_execute(bfly_buff);
-				//t2 = TCNT1; TCNT1 = 0;
-				fft_output(bfly_buff, spektrum);
-				//t3 = TCNT1;
-				for (n = 0; n < FFT_N / 2; n+=2) {
-					//s = (spektrum[n] + spektrum[n+1]) / 2;
-					s = spektrum[n];
-					//xmitf(PSTR("\r\n%4u:%5u "), n, s);
-					//char str[10];
-					//sprintf(str, "\n\r %d**",n);
-					//loggerWriteToMarker((LogMesT)str, '*');
-					loggerWrite("\n\r",2);
-					//s /= FFT_N;
-					for (m = 0; m < s; m++) loggerWrite("**",1);
-				}
-				//xmitf(PSTR("\r\ninput=%u, execute=%u, output=%u (x64clk)"), t1,t2,t3);
-
-				_delay_ms(1000);
-				break;
-
-			default :		/* Unknown command */
-				;//xmitstr(PSTR("\n???"));
-		}
+		fft_input(capture, bfly_buff);
+		fft_execute(bfly_buff);
+		fft_output(bfly_buff, spektrum);
+		
+		_delay_ms(50);
 	}
 }
